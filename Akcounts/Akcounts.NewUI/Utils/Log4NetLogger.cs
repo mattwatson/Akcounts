4d14a46b8bf72e6d1e40f0aca4a20a924691e5f0
Matt Watson <mattwatson1@gmail.com>
2013-03-03 13:12:41 +0000

    Added log4net via nuget

---
+++ Akcounts/Akcounts.NewUI/Utils/Log4NetLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Caliburn.Micro;
+
+namespace Akcounts.NewUI.Utils
+{
+    internal class Log4netLogger : ILog
+    {
+        private readonly log4net.ILog _logger;
+
+        public Log4netLogger(Type type)
+        {
+            _logger = log4net.LogManager.GetLogger(type);
+        }
+
+        public void Error(Exception exception)
+        {
+            _logger.Error(exception.Message, exception);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            _logger.InfoFormat(format, args);
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            _logger.WarnFormat(format, args);
+        }
+    }
+}
