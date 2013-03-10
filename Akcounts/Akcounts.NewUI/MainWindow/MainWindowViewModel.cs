0d46d63d585bc247a292c0b8e5597fc82f22996c
Matt Watson <mattwatson1@gmail.com>
2013-03-10 13:03:47 +0000

    Ui tweaks and added dummy data files to new Ui

--- Akcounts/Akcounts.NewUI/MainWindow/MainWindowViewModel.cs
+++ Akcounts/Akcounts.NewUI/MainWindow/MainWindowViewModel.cs
@@ -6,7 +6,6 @@ using Caliburn.Micro;
 namespace Akcounts.NewUI.MainWindow
 {
     [Export(typeof(IMainWindow))]
-    //TODO Change the IMainWindow below into the type of thing the MainWindow can conduct - e.g. IWorkspace
     public class MainWindowViewModel : Conductor<IWorkspace>.Collection.OneActive, IMainWindow
     {
         private static readonly ILog Log = LogManager.GetLog(typeof (MainWindowViewModel));
@@ -14,7 +13,7 @@ namespace Akcounts.NewUI.MainWindow
         [ImportingConstructor]
         public MainWindowViewModel([ImportMany] IEnumerable<IWorkspace> workspaces)
         {
-            Items.AddRange(workspaces); 
+            Items.AddRange(workspaces);
         }
     }
 }
